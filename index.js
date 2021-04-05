65a516dc6a6bf459121f8f76e4b14a7930a2f739
jennafritz <jennafritz24@gmail.com>
2021-04-05 12:05:48 -0400

    commit tree 9e2c56340c4726274f8c307dab1d6be6eb5d7534

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let greeting;
+  if (distance <= 400) {
+    greeting = "This one is on me!";
+  } else if (distance < 2500 && distance > 2000) {
+    greeting = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    greeting = "No can do.";
+  }
+  return greeting;
+  } 
+
+function ternaryCheckCity(destination){ 
+  let acceptance;
+  (destination === "NYC") ? (acceptance = "Ok, sounds good.") : (acceptance = "No go.");
+  return acceptance;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch (tip) {
+    case 'generous':
+      charm = "Thank you so much."
+      break;
+    case 'not as generous':
+      charm = "Thank you."
+      break;
+    default:
+      charm = "Bye."
+      break;
+    }
+  return charm;
+  }
